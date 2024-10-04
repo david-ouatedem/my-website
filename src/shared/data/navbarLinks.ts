e048dc256d0032010ec387a0f8862808564cb704
Jores Tala <jorestala16@gmail.com>
2024-10-05 00:57:59 +0100

    worked on layout

---
+++ src/shared/data/navbarLinks.ts
@@ -0,0 +1,18 @@
+export const navbarLinks = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "About",
+        path: "/about",
+    },
+    {
+        name: "Projects",
+        path: "/projects",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
+]
